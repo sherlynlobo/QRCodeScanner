1980fc703b0083244629a37070809bb9c4e5067d
sherlynlobo <sherlynlobo26@gmail.com>
2021-01-21 18:18:49 -0500

    added dependencies and camera permission

--- app/src/main/java/com/sl/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/sl/qrcodescanner/MainActivity.kt
@@ -2,11 +2,19 @@ package com.sl.qrcodescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.budiyev.android.codescanner.CodeScanner
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var codeScanner: CodeScanner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        codeScanner = CodeScanner(this,scannerview)
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
     }
 }

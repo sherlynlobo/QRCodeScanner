b281197cbf0d3f2e1edea10de70574e0fef4370b
sherlynlobo <sherlynlobo26@gmail.com>
2021-01-21 18:31:32 -0500

    functionality and error handling

--- app/src/main/java/com/sl/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/sl/qrcodescanner/MainActivity.kt
@@ -2,7 +2,12 @@ package com.sl.qrcodescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -16,5 +21,26 @@ class MainActivity : AppCompatActivity() {
         codeScanner = CodeScanner(this,scannerview)
         codeScanner.camera = CodeScanner.CAMERA_BACK
         codeScanner.formats = CodeScanner.ALL_FORMATS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Toast.makeText(this,"Scan Result: $(it.text)", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+        codeScanner.errorCallback = ErrorCallback {
+
+            runOnUiThread {
+                Toast.makeText(this,"Camera Error: $(it.message)",Toast.LENGTH_LONG).show()
+            }
+        }
+
+        codeScanner.startPreview()
     }
+
 }
